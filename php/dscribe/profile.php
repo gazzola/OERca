09ddf3292157c0614d8c18814fdf06b385bf71a1
Zhen Qian <zqian@umich.edu>
2007-09-28 18:41:54 +0000

    add FCKeditor suppport

--- php/dscribe/profile.php
+++ php/dscribe/profile.php
@@ -13,6 +13,9 @@ $TOOL_NAME="dScribe";
 $PAGE_NAME="Create/Update Profiles";
 
 ?>
+<?php
+include("../../fckeditor/fckeditor.php");
+?>
 <link href="../include/ocw_tool.css" rel="stylesheet" type="text/css"/>
 <div>&nbsp;&nbsp;&nbsp; <a href="index.php">dScribe Home</a>&nbsp;| &nbsp;Course &amp; Instructor Profiles&nbsp; | &nbsp;<a href="copyright.php">Set Default Copyright</a>&nbsp; | &nbsp;<a href="materials.php">Prepare Course Materials</a> &nbsp; | &nbsp;<a href="../preview/course.php">Review for Export</a></div>
 		<br/>
@@ -20,12 +23,33 @@ $PAGE_NAME="Create/Update Profiles";
 <div id="tool_content">
 <p>
 <strong>Edit Course Profile</strong></p><br/>
-&nbsp;&nbsp;&nbsp;<img src="../include/images/edit_courseInfo_JH.jpg" border="0"/>
-&nbsp;&nbsp;&nbsp;<input class="blue_submit" id="submitbutton" type="submit" name="login" value="save" tabindex="3" />   &nbsp;&nbsp;&nbsp;<input class="blue_submit" id="submitbutton" type="submit" name="login" value="cancel" tabindex="3" /></p>
+&nbsp;&nbsp;&nbsp;
+<form name="course" id="course" method="post" action="">
+<?php
+$oFCKeditor = new FCKeditor('FCKeditor1');
+$oFCKeditor->BasePath = '../../fckeditor/';
+$oFCKeditor->Value = 'Default text in editor';
+$oFCKeditor->Create();
+?>
+<br/>
+&nbsp;&nbsp;&nbsp;<input class="blue_submit" id="submitbutton" type="submit" name="login" value="save" tabindex="3" />   
+&nbsp;&nbsp;&nbsp;<input class="blue_submit" id="submitbutton" type="submit" name="login" value="cancel" tabindex="3" /></p>
+</form>
 <br/><br/>
 <p>
 <strong>Edit Instructor Profile</strong></p><br/>
-&nbsp;&nbsp;&nbsp;<img src="../include/images/edit_instructor_photo.jpg" border="0"/>&nbsp;&nbsp;&nbsp;<input class="blue_submit" id="submitbutton" type="submit" name="login" value="save" tabindex="3" />   &nbsp;&nbsp;&nbsp;<input class="blue_submit" id="submitbutton" type="submit" name="login" value="cancel" tabindex="3" /></p>
-<br/><br/><br/>
+&nbsp;&nbsp;&nbsp;
+<form name="instructor" id="instructor" method="post" action="">
+<?php
+$oFCKeditor = new FCKeditor('FCKeditor2');
+$oFCKeditor->BasePath = '../../fckeditor/';
+$oFCKeditor->Value = 'Default text in editor';
+$oFCKeditor->Create();
+?>
+<br/>
+&nbsp;&nbsp;&nbsp;<input class="blue_submit" id="submitbutton" type="submit" name="login" value="save" tabindex="3" /> 
+&nbsp;&nbsp;&nbsp;<input class="blue_submit" id="submitbutton" type="submit" name="login" value="cancel" tabindex="3" /></p>
+</form>
+<br/><br/>
 </div>
 </DIV>
