7781ac793fc1a16ab17aba2f353dc1f745fe9484
Zhen Qian <zqian@umich.edu>
2007-11-11 22:50:00 +0000

    correct image path

--- tool/system/application/views/default/content/instructor/pick_materials.php
+++ tool/system/application/views/default/content/instructor/pick_materials.php
@@ -1,7 +1,6 @@
 <?php $this->load->view(property('app_views_path').'/instructor/instructor_header.php', $data); ?>
 <?php $this->load->view(property('app_views_path').'/OCWItem.php', $data); ?>
 <?php $this->load->view(property('app_views_path').'/OCWItemList.php', $data); ?>
-
 <div id="tool_content">
 <?php
 $this->CI = $this->freakauth_light;
@@ -29,12 +28,6 @@ $TOOL_NAME="Instructor";
 $PAGE_NAME="Manager Course Materials";
 
 ?>
-<style type='text/css'>
-<?php $this->file(BASEPATH.'application/views/ocw_tool.css'); ?>
-</style>
-<div style="text-align:left; margin-bottom:20px;  ">
-
-<br/>
 <form name="chooseMaterialForm" method="post" action="">
 <table border="0" cellpadding="0" cellspacing="0" class="course_materials">
 <tr>
@@ -51,11 +44,14 @@ $PAGE_NAME="Manager Course Materials";
 			Then, click on <strong>Add</strong> to update the OCW materials list on the right. <br/><br/></div>
 		<div class="materials_list">
 			<?php 
-				//echo "$result1";
 				foreach ($toolTitles as $title)
 				{
-					echo "<div class='paren'><img src='../include/images/validated.gif' height='15' /> &nbsp;&nbsp;
-						<img src='../include/images/page.png' height='15' /> &nbsp;&nbsp; $title<br />";
+					echo "<div class='paren'>";
+					$imageUrl=property('app_img').'/validated.gif';
+					echo "<img src=\"".$imageUrl."\""."  height='15' />"."&nbsp;&nbsp;";
+					$imageUrl=property('app_img').'/page.png';
+					echo "<img src=\"".$imageUrl."\""."  height='15' />"."&nbsp;&nbsp;";
+					echo $title."<br />";
 					if ($title == "Assignments")
 					{
 						// reading the assignment list xml string
@@ -66,7 +62,6 @@ $PAGE_NAME="Manager Course Materials";
 						{
 							$assignmentIds = $assignment->getElementsByTagName("AssignmentId");
 							$assignmentId = $assignmentIds->item(0)->nodeValue;
-							//echo "id=$assignmentId    ";
 							$assignmentTitles = $assignment->getElementsByTagName("AssignmentTitle");
 							$assignmentTitle = $assignmentTitles->item(0)->nodeValue;
 							echo "<div> &nbsp;&nbsp; <input type='checkbox' name='chooseItem'>&nbsp;&nbsp; $assignmentTitle <a href='#' title='add only this item'>( Add )</a><br />";
@@ -106,81 +101,41 @@ $PAGE_NAME="Manager Course Materials";
 				}
 				unset($value); // break the reference with the last element
 			?>
-			<?php
-			/*<div class="parent"><img src="../include/images/validated.gif" height="15" /> &nbsp;&nbsp;<img src="../include/images/page.png" height="15" /> &nbsp;&nbsp; 	Syllabus</div>
-			
-		
-		
-		
-			
-			<div class="parent checked"> <input type="checkbox" name="checked[]" value="<?=$proposal['pk']?>" />	<img src="../include/images/folder.gif" height="18" /> &nbsp;&nbsp;	Lectures
-			
-				<div class="child checked">
-					<div> <img src="../include/images/validated.gif" height="15" /> <img src="../include/images/ppt.jpg" height="15" /> &nbsp;&nbsp; SI514 L1 W2007.ppt</div>
-					<div> <img src="../include/images/validated.gif" height="15" /> <img src="../include/images/ppt.jpg" height="15" /> &nbsp;&nbsp; SI514 L2 W2007.ppt</div>
-					<div> <img src="../include/images/validated.gif" height="15" /> <img src="../include/images/ppt.jpg" height="15" /> &nbsp;&nbsp; SI514 L3 W2007.ppt</div>
-					<div> <img src="../include/images/validated.gif" height="15" /> <img src="../include/images/ppt.jpg" height="15" /> &nbsp;&nbsp; SI514 L4 W2007.ppt</div>
-					<div> <img src="../include/images/validated.gif" height="15" /> <img src="../include/images/ppt.jpg" height="15" /> &nbsp;&nbsp; SI514 L5 W2007.ppt</div>
-					<div> <input type="checkbox" name="checked[]" value="<?=$proposal['pk']?>" /> <img src="../include/images/ppt.jpg" height="15" /> &nbsp;&nbsp; SI514 L6 W2007.ppt</div>
-				<div> <input type="checkbox" name="checked[]" value="<?=$proposal['pk']?>" /> <img src="../include/images/ppt.jpg" height="15" /> &nbsp;&nbsp; SI514 L7 W2007.ppt</div>
-				</div>
-		   </div>
-			<div class="parent">
-				<img src="../include/images/blank.gif" height="3" /><input type="checkbox" name="checked[]" value="<?=$proposal['pk']?>" />
-					 <img src="../include/images/page.png" height="15" />&nbsp;&nbsp;Readings</div>
-				
-			
-			
-			<div class="parent checked">
-			<img src="../include/images/validated.gif" height="15" />&nbsp;&nbsp;&nbsp;&nbsp;Assignments</div>
-			
-			<div class="parent"><img src="../include/images/blank.gif" height="3" /><input type="checkbox" name="checked[]" value="<?=$proposal['pk']?>" />
-				&nbsp;&nbsp;Schedule</div>	
-			<div class="parent checked">	 
-			<img  src="../include/images/exclaim.gif"> Lecture 1 video<span style="color:red; font-size: .95em;" title="this item has been changed">
-				&nbsp;&nbsp;- item has been modified </span> </div>
-				*/?>
-			
 		</div>
 	</td>
 		
  <td style="width: 10%; vertical-align:top;">
 	
-	<table border="0" style="font-weight: normal">
-		<tr>
-			<td class="text"></td>
-			<td><br/><br/><br/><br/><br/>&nbsp;&nbsp;&nbsp;<input class="blue_submit" id="submitbutton" type="submit" name="login" value="Add >>>" tabindex="3" /></td>
-		</tr>
-	</table>
-	</form><br/><br/>
+	<br/><br/><br/><br/><br/>&nbsp;&nbsp;&nbsp;
+	<input class="blue_submit" id="submitbutton" type="submit" name="login" value="Add >>>" tabindex="3" /></td>
+
 </td>
 
 <td width=40% style="vertical-align:top; font-weight: normal; text-align: left; border:1px solid #ccc; ">
  <div class="materials_list">
  <p class="instructions"><br/>To remove materials from this OCW materials list  - click on the <strong>remove</strong> link for that item.<br/><br/></p>
 	
-<div class="parent">&nbsp; <img src="../include/images/page.png" height=15  /> &nbsp;&nbsp;Syllabus <a href="#" title="remove this item">( remove )</a>
+<div class="parent">&nbsp; <img src="<?= property('app_img').'/page.png'?>" height=15  /> &nbsp;&nbsp;Syllabus <a href="#" title="remove this item">( remove )</a>
 </div>
 
-<div class="parent">&nbsp; <img src="../include/images/folder.gif" height=15  /> &nbsp;&nbsp;Lectures <a href="#" title="remove this item and all child items">( remove )</a>
+<div class="parent">&nbsp; <img src="<?= property('app_img').'/folder.gif'?>" height=15  /> &nbsp;&nbsp;Lectures <a href="#" title="remove this item and all child items">( remove )</a>
 
 <div class="child">
-	<div><img src="../include/images/blank.gif" height="15" /><img src="../include/images/ppt.jpg" height="15" /> &nbsp;&nbsp; SI514 L1 W2007.ppt <a href="#" title="remove only this item">( remove )</a></div>
-<div> <img src="../include/images/blank.gif" height="15" /><img src="../include/images/ppt.jpg" height="15" /> &nbsp;&nbsp; SI514 L2 W2007.ppt <a href="#" title="remove only this item">( remove )</a></div>
-<div> <img src="../include/images/blank.gif" height="15" /><img src="../include/images/ppt.jpg" height="15" /> &nbsp;&nbsp; SI514 L3 W2007.ppt <a href="#" title="remove only this item">( remove )</a></div>
-<div> <img src="../include/images/blank.gif" height="15" /><img src="../include/images/ppt.jpg" height="15" /> &nbsp;&nbsp; SI514 L4 W2007.ppt <a href="#" title="remove only this item">( remove )</a></div>
-<div> <img src="../include/images/blank.gif" height="15" /><img src="../include/images/ppt.jpg" height="15" /> &nbsp;&nbsp; SI514 L5 W2007.ppt <a href="#" title="remove only this item">( remove )</a></div>
-</div>
-	</div>
+	<div><img src="<?= property('app_img').'/blank.gif'?>" height="15" /><img src="<?= property('app_img').'/ppt.jpg'?>" height="15" /> &nbsp;&nbsp; SI514 L1 W2007.ppt <a href="#" title="remove only this item">( remove )</a></div>
+	<div><img src="<?= property('app_img').'/blank.gif'?>" height="15" /><img src="<?= property('app_img').'/ppt.jpg'?>" height="15" /> &nbsp;&nbsp; SI514 L2 W2007.ppt <a href="#" title="remove only this item">( remove )</a></div>
+	<div><img src="<?= property('app_img').'/blank.gif'?>" height="15" /><img src="<?= property('app_img').'/ppt.gif'?>" height="15" /> &nbsp;&nbsp; SI514 L3 W2007.ppt <a href="#" title="remove only this item">( remove )</a></div>
+	<div><img src="<?= property('app_img').'/blank.gif'?>" height="15" /><img src="<?= property('app_img').'/ppt.jpg'?>" height="15" /> &nbsp;&nbsp; SI514 L4 W2007.ppt <a href="#" title="remove only this item">( remove )</a></div>
+	<div><img src="<?= property('app_img').'/blank.gif'?>" height="15" /><img src="<?= property('app_img').'/ppt.jpg'?>" height="15" /> &nbsp;&nbsp; SI514 L5 W2007.ppt <a href="#" title="remove only this item">( remove )</a></div>
+
 <div class="parent">
-<img src="../include/images/blank.gif" height="15" />&nbsp;&nbsp;&nbsp;&nbsp;Assignments
+	<div><img src="<?= property('app_img').'/blank.gif'?>" height="15" />&nbsp;&nbsp;&nbsp;&nbsp;Assignments
  <a href="#" title="remove only this item">( remove )</a></div>
 
 	
 <div class="parent">	 
-<img  src="../include/images/exclaim.gif"> Lecture 1 video <span style="color:red; font-size: .95em;" title=" item has been changed">&nbsp;&nbsp;- item has been modified</span> </div>
+	<img src="<?= property('app_img').'/exclaim.gif'?>"> Lecture 1 video <span style="color:red; font-size: .95em;" title=" item has been changed">&nbsp;&nbsp;- item has been modified</span> </div>
 	</div>
 	</td></tr>
  
 </table>
-</div>
+</form>
