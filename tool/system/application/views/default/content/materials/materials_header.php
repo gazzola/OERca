c0689c396852c3e70a3b801041444f96628b1b3f
David Hutchful <dkhutch@umich.edu>
2008-02-12 20:31:53 +0000

    updating content object page

--- tool/system/application/views/default/content/materials/materials_header.php
+++ tool/system/application/views/default/content/materials/materials_header.php
@@ -1,5 +1,3 @@
-<h1><?=$cname?><?php if (isset($material['name'])) { echo '&nbsp;&raquo;&nbsp;'. $material['name']; } ?></h1><br/>
-
 <div class="column span-24 first last">
 
     <div id="navlist">
@@ -12,7 +10,7 @@
 			<li><?=anchor("/instructor/dscribes/$cid",$this->lang->line('ocw_ins_menu_manage'))?></li>
 <?php } elseif ($caller=='dscribe1') { ?>
 			<li><?=anchor("/dscribe1/home/$cid",$this->lang->line('ocw_ds_menu_home'))?></li>
-			<li id="active"><a href=""><?=$this->lang->line('ocw_ds_menu_materials')?></a></li>
+			<li id="active"><a href="">Manage</a></li>
 			<li><?=anchor("/dscribe1/profiles/$cid",$this->lang->line('ocw_ds_menu_profiles'))?></li>
 			<li><?=anchor("/dscribe1/copyright/$cid",$this->lang->line('ocw_ds_menu_copyright'))?></li>
 			<li><?=anchor("/dscribe1/tags/$cid",$this->lang->line('ocw_ds_menu_tags'))?></li>
@@ -20,14 +18,24 @@
 <?php } ?>
 		</ul>
 	</div>
-<!--
 	<br/>
 
-<?php if (isset($material['name'])) { ?>
 	<div style="border-bottom: 1px solid #eee; margin-top: -10px; margin-left: 5px; padding-bottom: 5px;">
-		<a href="<?=site_url('materials/home/'.$cid.'/'.$caller)?>"><?=$cname?> Materials</a>
-		&nbsp;&raquo;&nbsp;<b><?=$material['name']?></b>&nbsp;&raquo;&nbsp;Content objects
+    <div style="float: left">
+		    <a href="">Courses</a> &raquo; <a href="<?=site_url('materials/home/'.$cid.'/'.$caller)?>"><?=$cname?></a> &raquo;  
+        <?php echo (isset($material['name'])) ? $material['name'] : 'materials'; ?>
+    </div>
+    <div id="materials_nav" style="float: right">
+		  <ul>
+			    <li><a id="do_edit_mat_info">Edit Material Info</a></li>
+			    <li><a id="do_view_mat_comm">View Material Comments</a></li>
+			    <li><a id="do_upload_conobj">Upload Content Object</a></li>
+			    <li><a href="<?=$material['ctools_url']?>">Download Material</a></li>
+      </ul>
+    </div>
+     <div style="clear:both"></div>
+     <script type="text/javascript">
+   
+    </script>
 	</div>
-<?php } ?>
--->
 <br/>
