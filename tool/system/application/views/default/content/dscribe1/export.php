57e03c8f4e462d64fefe114561df8fd87fc0b206
Zhen Qian <zqian@umich.edu>
2008-02-14 20:38:54 +0000

    add the export page

---
+++ tool/system/application/views/default/content/dscribe1/export.php
@@ -0,0 +1,22 @@
+<?php $this->load->view(property('app_views_path').'/dscribe1/dscribe1_header.php', $data); ?>
+<?php
+echo form_open('dscribe1/export', '', '');
+?>
+<table border="0" cellpadding="0" cellspacing="0">
+<tr>
+	<td width="40%" style="font-weight: bold; text-align: left; border:1px solid #ccc;padding: 5px;  ">
+		Export Course
+    </td>
+ </tr>
+  <tr>
+	<td style="font-weight: bold; text-align: left; border:1px solid #ccc;padding: 5px;  ">
+		<h4><?php echo $this->lang->line('ocw_ds_export_instruction') ?></h4>
+	</td>
+  </tr>
+</table>
+<?php
+		echo form_submit('exportbutton', 'Export');
+		$string = "</div></div>";
+		echo form_close($string);
+?>
+<?php $this->load->view(property('app_views_path').'/dscribe1/dscribe1_footer.php', $data); ?>
