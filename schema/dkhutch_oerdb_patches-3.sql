68a61baa6ca9b22b282a81b49cd1c1d51073887f
David Hutchful <dkhutch@umich.edu>
2008-05-08 21:26:51 +0000

    Added changes to action enum fields for claim tables

--- schema/dkhutch_oerdb_patches-3.sql
+++ schema/dkhutch_oerdb_patches-3.sql
@@ -1,8 +1,8 @@
-ALTER TABLE `ocw_claims_commission` CHANGE `action` `action` ENUM( 'None', 'Permission', 'Search', 'Fair Use', 'Re-Create', 'Retain: Instructor Created', 'Retain: Public Domain', 'Retain: No Copyright', 'Remove & Annotate' ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT 'None' 
+ALTER TABLE `ocw_claims_commission` CHANGE `action` `action` ENUM( 'None', 'Permission', 'Search', 'Fair Use', 'Re-Create', 'Retain: Instructor Created', 'Retain: Public Domain', 'Retain: No Copyright', 'Remove & Annotate' ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT 'None'; 
 
- ALTER TABLE `ocw_claims_fairuse` CHANGE `action` `action` ENUM( 'None', 'Permission', 'Search', 'Re-Create', 'Retain: Instructor Created', 'Retain: Public Domain', 'Retain: No Copyright', 'Commission', 'Remove & Annotate' ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT 'None' 
+ ALTER TABLE `ocw_claims_fairuse` CHANGE `action` `action` ENUM( 'None', 'Permission', 'Search', 'Re-Create', 'Retain: Instructor Created', 'Retain: Public Domain', 'Retain: No Copyright', 'Commission', 'Remove & Annotate' ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT 'None'; 
 
- ALTER TABLE `ocw_claims_permission` CHANGE `action` `action` ENUM( 'None', 'Search', 'Fair Use', 'Re-Create', 'Retain: Instructor Created', 'Retain: Public Domain', 'Retain: No Copyright', 'Commission', 'Remove & Annotate' ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT 'None' 
+ ALTER TABLE `ocw_claims_permission` CHANGE `action` `action` ENUM( 'None', 'Search', 'Fair Use', 'Re-Create', 'Retain: Instructor Created', 'Retain: Public Domain', 'Retain: No Copyright', 'Commission', 'Remove & Annotate' ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT 'None'; 
 
- ALTER TABLE `ocw_claims_retain` CHANGE `action` `action` ENUM( 'None', 'Permission', 'Search', 'Fair Use', 'Re-Create', 'Retain: Instructor Created', 'Retain: Public Domain', 'Commission', 'Remove & Annotate' ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT 'None' 
+ ALTER TABLE `ocw_claims_retain` CHANGE `action` `action` ENUM( 'None', 'Permission', 'Search', 'Fair Use', 'Re-Create', 'Retain: Instructor Created', 'Retain: Public Domain', 'Commission', 'Remove & Annotate' ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT 'None'; 
 
