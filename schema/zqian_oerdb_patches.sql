24ac06200d944b3fc10ac10e7238ad56684c01b7
Zhen Qian <zqian@umich.edu>
2008-05-15 14:07:32 +0000

    fix to OERDEV-8:recommended action dropdown box - no save

---
+++ schema/zqian_oerdb_patches.sql
@@ -0,0 +1,1 @@
+ALTER TABLE `ocw_objects` CHANGE `action_type` `action_type` ENUM( 'Permission','Search','Fair Use', 'Re-Create','Retain: Instructor Created', 'Retain: Public Domain', 'Retain: No Copyright', 'Commission', 'Remove & Annotate' ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT 'Search'; 
