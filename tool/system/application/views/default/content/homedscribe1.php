90efa1e20516df1f7429e863ec43272fcb8e74e0
Ali Asad Lotia <lotia@umich.edu>
2008-06-03 20:15:12 +0000

    Display message to dscribe1 when 0 CO's

--- tool/system/application/views/default/content/homedscribe1.php
+++ tool/system/application/views/default/content/homedscribe1.php
@@ -1,16 +1,27 @@
 <h1><?=$name . "'s"?> Status</h1><br />
 
-  <?php if ($courses) { ?>
-  <div class="column span-24 first last">
-    <h2>Cleared <img src="<?= site_url("/home/make_stat_key/done") ?>" class="prog-key">
-    In Progress <img src="<?= site_url("/home/make_stat_key/ask") ?>" class="prog-key"> 
-    Not Cleared <img src="<?= site_url("/home/make_stat_key/rem") ?>" class="prog-key"> </h2>
-    
-  </div>
-  <?  foreach ($courses as $key => $value) { 
-       $params_url = $value['num']['total'].'/'.$value['num']['done']. 
+  <?php
+  $printkey = FALSE; 
+  if ($courses) { 
+    foreach ($courses as $value) {
+      if ($value['num']['total'] > 0) {
+        $printkey = TRUE;
+        break;
+      }
+    }
+    if ($printkey == TRUE) { // print the key only if we have COs ?> 
+      <div class="column span-24 first last">
+        <h2>
+          Cleared <img src="<?= site_url("/home/make_stat_key/done") ?>" class="prog-key">
+          In Progress <img src="<?= site_url("/home/make_stat_key/ask") ?>" class="prog-key"> 
+          Not Cleared <img src="<?= site_url("/home/make_stat_key/rem") ?>" class="prog-key">
+        </h2>  
+      </div>
+  <?php } ?>
+  <? foreach ($courses as $value) {
+        $params_url = $value['num']['total'].'/'.$value['num']['done']. 
               '/'.$value['num']['ask'].'/'.$value['num']['rem'];
-   ?> 
+      if ($value['num']['total'] > 0) { ?> 
   <div class="column span-16 first">
     <a href="<?php echo site_url("materials/home/{$value['id']}"); ?>" >
       <img src="<?= site_url("/home/make_bar/$params_url") ?>" 
@@ -29,7 +40,14 @@
         </a>
     </h2>
   </div>
-  <?php }} else { ?>
+  <?php } else { ?>
+  <div class="column span-24 first last" style="margin-bottom: 10px">
+    <h2><a href="<?php echo site_url("materials/home/{$value['id']}"); ?>" >
+        <?=$value['number'] ?> <?=$value['title'] ?>
+        </a> does not contain any content objects.<br />
+    </h2>
+  </div>
+  <?php  }}} else { ?>
   <div class="column span-24 first last">
     You have no courses at present. Ask one of the staff to assign a course.
   </div>
