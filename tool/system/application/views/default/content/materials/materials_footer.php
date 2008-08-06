6176e95e7de0bf412fc0c73ef778e34793393ab7
Zhen Qian <zqian@umich.edu>
2008-08-06 14:01:35 +0000

    fix to OERDEV-161: Javascript error in the content object view
    Check for the existence of javascript object first.

--- tool/system/application/views/default/content/materials/materials_footer.php
+++ tool/system/application/views/default/content/materials/materials_footer.php
@@ -5,14 +5,17 @@ window.addEvent('domready', function() {
 	var myTips = new MooTips($$('.ine_tip'), { maxTitleChars: 50 }); 
 	<?php if (isset($oid)) { ?>
 	// open window to edit CO information
-	$('edit-<?=$oid?>').addEvent('openeditor', function() {
-  		this.blur();
-  		var caption = this.title || this.name || "";
-  		var group = this.rel || false;
-  		TB_show(caption, this.href, group);
-  		this.onclick=TB_bind;
-	}); 	
-	$('edit-<?=$oid?>').fireEvent('openeditor');	
+	if ($('edit-<?=$oid?>') != null)
+	{
+		$('edit-<?=$oid?>').addEvent('openeditor', function() {
+	  		this.blur();
+	  		var caption = this.title || this.name || "";
+	  		var group = this.rel || false;
+	  		TB_show(caption, this.href, group);
+	  		this.onclick=TB_bind;
+		});	
+		$('edit-<?=$oid?>').fireEvent('openeditor');
+	}	
 	<?php } ?>
 });
 </script>
