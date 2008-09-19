310a26bc0b30a4d6a40371460b67a5ddd86ec079
Kevin Coffman <kwc@umich.edu>
2008-09-19 20:06:59 +0000

    OERDEV-187
    Update the 'modified_on' for a material any time something in the material is updated.

--- tool/system/application/models/material.php
+++ tool/system/application/models/material.php
@@ -312,6 +312,12 @@ class Material extends Model
     */
   public function update($mid, $data)
   {
+		// We're modifying the material.  Update the 'modified_on' time.
+		// It seems we could get this for free from the DB code by
+		// making a schema change?
+		echo "Hi Piet<br>";
+		if (!isset($data['modified_on']))
+			$data['modified_on'] = date('Y-m-d h:i:s');
     $this->db->update('materials',$data,"id=$mid");
   }
 
