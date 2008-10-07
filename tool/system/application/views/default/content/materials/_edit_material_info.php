8e936d458d9c76fb9ed5efa6a70ec1d9b7274bb6
Kevin Coffman <kwc@umich.edu>
2008-10-07 21:32:47 +0000

    OERDEV-177
    When editing a material's information, if the material has Content Objects,
    disable the "Embedded COs?" radio button.  In this case, it should be marked
    as "yes" and can only be changed to "no" if all the COs for the material have
    been deleted.
    
    If the material has no COs, leave the button enabled to go between "yes" and "no".

--- tool/system/application/views/default/content/materials/_edit_material_info.php
+++ tool/system/application/views/default/content/materials/_edit_material_info.php
@@ -51,9 +51,13 @@ $copyholder = ($material['author']=='') ? $course['director'] : $material['autho
 		<br/>
 
 		<div class="formField">Embedded COs?:&nbsp;&nbsp;
-			<?php if ($material['embedded_co']==1) { ?>
-        		<input type="radio" name="embedded_co" id="emip_yes" class="update_material" value="1" checked="checked" />&nbsp;Yes
-        		<input type="radio" name="embedded_co" id="emip_no" class="update_material" value="0" />&nbsp;No
+			<?php if ($material['embedded_co']==1) {
+						if ($co_count != 0)
+							$state = 'disabled="disabled"';
+						else
+						 	$state = ''; ?>
+        		<input type="radio" name="embedded_co" id="emip_yes" class="update_material" value="1" checked="checked" <?=$state?> />&nbsp;Yes
+        		<input type="radio" name="embedded_co" id="emip_no" class="update_material" value="0" <?=$state?> />&nbsp;No
     		<?php } else { ?>
         		<input type="radio" name="embedded_co" id="emip_yes" class="update_material" value="1" />&nbsp;Yes
         		<input type="radio" name="embedded_co" id="emip_no" class="update_material" value="0" checked="checked" />&nbsp;No
