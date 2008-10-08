856ba0f8f8ecaa0e5cb460da9bcf3643e3ace31e
Ali Asad Lotia <lotia@umich.edu>
2008-10-08 21:41:47 +0000

    Remove white titlebar on smoothbox panels
    OERDEV-195. This is part one of a two part fix.
    The form header test was repeated at the top of the smoothboxes.
    This text was inconsistent in some cases and redundant in
    others. Since this deals with a javascript and the rest of the
    fix is edits to forms, I decided to make two separate commits.

--- tool/assets/tool2/script/smoothbox.js
+++ tool/assets/tool2/script/smoothbox.js
@@ -233,7 +233,10 @@ function TB_show(caption, url, rel) {
 		if(url.indexOf('TB_iframe') != -1){				
 			urlNoQuery = url.split('TB_');		
 			//$("TB_window").innerHTML += "<div id='TB_title'><div id='TB_ajaxWindowTitle'>"+caption+"</div><div id='TB_closeAjaxWindow'><a href='#' id='TB_closeWindowButton' title='Close'>close</a></div></div><iframe frameborder='0' hspace='0' src='"+urlNoQuery[0]+"' id='TB_iframeContent' name='TB_iframeContent' style='width:"+(ajaxContentW + 29)+"px;height:"+(ajaxContentH + 17)+"px;' onload='TB_showWindow()'> </iframe>";
-			$("TB_window").innerHTML += "<div id='TB_title'><div id='TB_ajaxWindowTitle'>"+caption+"</div></div><iframe frameborder='0' hspace='0' src='"+urlNoQuery[0]+"' id='TB_iframeContent' name='TB_iframeContent' style='width:"+(ajaxContentW + 29)+"px;height:"+(ajaxContentH + 17)+"px;' onload='TB_showWindow()'> </iframe>";
+			//$("TB_window").innerHTML += "<div id='TB_title'><div id='TB_ajaxWindowTitle'>"+caption+"</div></div><iframe frameborder='0' hspace='0' src='"+urlNoQuery[0]+"' id='TB_iframeContent' name='TB_iframeContent' style='width:"+(ajaxContentW + 29)+"px;height:"+(ajaxContentH + 17)+"px;' onload='TB_showWindow()'> </iframe>";
+			// remove the white TB_title and TB_ajaxWindowTitle elements in the popup smoothbox
+			$("TB_window").innerHTML += "<iframe frameborder='0' hspace='0' src='"+urlNoQuery[0]+"' id='TB_iframeContent' name='TB_iframeContent' style='width:"+(ajaxContentW + 29)+"px;height:"+(ajaxContentH + 17)+"px;' onload='TB_showWindow()'> </iframe>";
+			
 		} else {
 			$("TB_window").innerHTML += "<div id='TB_title'><div id='TB_ajaxWindowTitle'>"+caption+"</div><div id='TB_closeAjaxWindow'><a href='#' id='TB_closeWindowButton'>close</a></div></div><div id='TB_ajaxContent' style='width:"+ajaxContentW+"px;height:"+ajaxContentH+"px;'></div>";
 		}
