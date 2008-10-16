ac309f51ed72e2bed6c09307ae6fc32d5a144e29
Ali Asad Lotia <lotia@umich.edu>
2008-10-16 17:46:49 +0000

    Add comments to database update script.

--- dbscripts/update_action_items_1.sql
+++ dbscripts/update_action_items_1.sql
@@ -1,3 +1,15 @@
+/*  this script changes the database schema to reflect the
+    changes made to the code in SVN r53972. The changes are
+    related to the ENUM values assigned to action, action_type
+    and action_taken columns. It also updates the relevant
+    records so that they have the new values instead of the
+    old, deleted ENUM values. */
+
+/*  variables may have been used to reduce repeated code
+    but chances of making a mistake are increased when
+    trying to do the clever stuff.
+    TODO: consider making this cleaner */
+
 ALTER TABLE `ocw_claims_commission` MODIFY COLUMN `action` 
       ENUM ( 'None',
      'Permission',
