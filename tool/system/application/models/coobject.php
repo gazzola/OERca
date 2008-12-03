929001b4df1d3b04439e42e6c972b1ebf5f6908a
Michael Bleed <mbleed@umich.edu>
2008-12-03 16:53:23 +0000

    Add check for illegal material id in co objct display

--- tool/system/application/models/coobject.php
+++ tool/system/application/models/coobject.php
@@ -121,7 +121,7 @@ class Coobject extends Model
 	public function coobjects($mid, $oid='', $action_type='', $details='*')
 	{
 		$objects = array();
-		($mid == ":") ? $where = "material_id=0" : $where = "material_id=$mid";
+		$where = (is_numeric($mid)) ? "material_id=$mid" : "material_id=0";
 		// retrieve COs sorted by location
 		$sort_order = "CAST(location AS UNSIGNED) ASC, location ASC, modified_on ASC";
 
