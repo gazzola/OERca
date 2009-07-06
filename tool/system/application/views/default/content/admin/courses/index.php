343b382f16b6b84f876480cab979d9a24b253d5a
Kevin Coffman <kwc@umich.edu>
2009-07-06 21:30:10 +0000

    Remove special case of admin course view with no courses

--- tool/system/application/views/default/content/admin/courses/index.php
+++ tool/system/application/views/default/content/admin/courses/index.php
@@ -1,17 +1,10 @@
 <?php $this->load->view(property('app_views_path').'/admin/admin_header.php', $data); ?>
 
 <div class="column span-24 first last" style="margin-bottom: 10px;">
-<?php if ($courses == null || !isset($courses) ) { ?>
-
-	<p class="error">No courses found:&nbsp;&nbsp;<a href="<?=site_url("courses/add_new_course/")?>?TB_iframe=true&height=675&width=875" class="smoothbox" title="Add a new Course" style="color:blue">Add a Course</a></p>
-
-<?php } else { ?>
 	
 	<?php	// Simply load the existing courses page
 				$this->load->view(property('app_views_path').'/courses/index.php', $data);
 	?>
-
-<?php } ?>
 
 </div>
 
