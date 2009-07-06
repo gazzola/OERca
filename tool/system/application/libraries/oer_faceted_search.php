e0d016fc0bb4e4d9b63b062f9a4868f717d88c61
Ali Asad Lotia <lotia@umich.edu>
2009-07-06 20:46:34 +0000

    Add course facet filtering to oer_faceted_search.
    The values returned by the course->new_get_courses model function can be
    filtered with the oer_faceted_search->do_course_facet_filtering library
    function.
    
    The view code has still not been fixed up and so the filtering functionality
    has only been lightly tested using test functions that return the raw data.
    
    There are some bugs in the view and controller code, particularly concerning
    the parsing of the url and the resulting function parameters that are holding
    up the faceted search functionality. These MUST be fixed in order to get
    functioning faceted search.

--- tool/system/application/libraries/oer_faceted_search.php
+++ tool/system/application/libraries/oer_faceted_search.php
@@ -118,7 +118,7 @@ class OER_faceted_search
       }
     }
     
-    // sort so the options always appear in the same order
+    // Sort arrays so the options always appear in the same order.
     asort($instructors);
     asort($dscribe1s);
     asort($dscribe2s);
@@ -133,6 +133,228 @@ class OER_faceted_search
     $facet_options['schools'] = $schools;
     
     return($facet_options);
+  }
+  
+  
+  /**
+    * Filters the course information returned by the new_get_courses function 
+    * from the course model according to the faceted search parameters.
+    * 
+    * The current logic used is that any of the parameters within the same
+    * facet category must be satisfied which implies an OR relationship
+    * within a facet category. However at least one filter from each facet
+    * category MUST be satisfied which implies an AND relationship between
+    * categories. If all specified filter categories don't match the course
+    * it is removed from the list.
+    * 
+    * This function calls filter specific functions to do the filtering.
+    * The sub functions are called based on the filter names. So to add more
+    * filters it is necessary to also add an appropriate filtering function.
+    * 
+    * The course array is passed by reference so it is edited in place.
+    *
+    * @access public
+    * @param Course information in the format returned by new_get_course
+    * @param Array of filter parameters. The each filter value within a 
+    *        category is separated by a 'z' which we use as the token
+    *        across which to split filters.
+    * @return void. Since the courses array is passed by reference which is
+    *         allowing editing in place.
+    */
+  public function do_course_facet_filtering(&$courses, $filters)
+  {
+    /* List of valid filter names. These values match up to the allowed
+     * filter facets for the facted search. This filter is a check to prevent
+     * invalid "facets" in the filters parameter array from messing up the 
+     * result set.
+     *
+     * The legal facet names in turn match up with the keys in the
+     * course level subarray of the $courses parameter. See the 
+     * new_get_courses function in the course model for more information.
+     *
+     * If a filter "facet"/category is added to the course faceted search
+     * the equivalent key name should be added to this array. Otherwise the
+     * "facet"/category will not work.
+     */
+    $valid_filters = array(
+      "school_id",
+      "term",
+      "year",
+      "dscribe2s",
+      "dscribe1s"
+      );
+      
+      // filter arguments are passed as strings separated by this character
+      $param_separator = "z";
+      
+      $current_filters = array();
+      
+      $props_to_check = array();
+      
+    /* Narrow the "facet" filtering to only those that have non-zero values.
+     * Also check to see if it is an "allowed" filter by checking against
+     * the array above. 
+     */
+    foreach ($filters as $filter_name => $filter_value) {
+      if (!empty($filter_value) && in_array($filter_name,
+        $valid_filters)) {
+        $current_filters[$filter_name] = $filter_value;
+      }
+    }
+    
+    /*
+     * Traverse the courses array, match the filter keys against the key names
+     * of the course properties so we only match against those. If we don't get
+     * a match, delete the course from the $courses array.
+     *
+     * The appropriate filter function is called using the property name value
+     * to formulate the function call statement.
+     */
+    foreach ($courses as $school => $curriculum) {
+      foreach ($curriculum as $curr_name => $curr_courses) {
+        foreach ($curr_courses as $cid => $cprops) {
+          $props_to_check = array_intersect_key($cprops, $current_filters);
+          foreach ($props_to_check as $prop_name => $prop_vals) {
+            $filter_function = "__do_{$prop_name}_filter";
+            if ($this->$filter_function($prop_vals, 
+              split($param_separator, $current_filters[$prop_name])) == 1) {
+              unset($courses[$school][$curr_name][$cid]);
+              break 1;
+            }
+          }
+        }
+        if (count($courses[$school][$curr_name]) == 0) {
+          unset($courses[$school][$curr_name]);
+        }
+      }
+      if (count($courses[$school]) == 0) {
+        unset($courses[$school]);
+      }
+    }
+  }
+  
+    
+  /**
+    * Filter on dscribe1 user ids.
+    *
+    * @access private
+    * @param array of dscribe1s where the user id is the key.
+    * @param array of numeric dscribe1 user ids to match against.
+    * @return 0 on successful match. 1 otherwise.
+    */ 
+  private function __do_dscribe1s_filter($dscribe1s, $chosen_dscribe1s)
+  {
+    return $this->__do_multi_key_filter($dscribe1s, $chosen_dscribe1s);
+  }
+  
+  
+  /**
+    * Filter on dscribe2 user ids.
+    *
+    * @access private
+    * @param array of dscribe1s where the user id is the key.
+    * @param array of numeric dscribe2 user ids to match against.
+    * @return 0 on successful match. 1 otherwise.
+    */ 
+  private function __do_dscribe2s_filter($dscribe2s, $chosen_dscribe2s)
+  {
+    return $this->__do_multi_key_filter($dscribe2s, $chosen_dscribe2s);
+  }
+  
+  
+  /**
+    * Filter on array key. If a match is found, return 0 otherwise return 1.
+    * 
+    * @access private
+    * @param array of users where the user id is the key.
+    * @param array of numeric user ids to filter against.
+    * @return 0 on successful match. 1 otherwise.
+    */
+  private function __do_multi_key_filter($property_list, $filter_vals)
+  {
+    if (count(array_intersect(array_keys($property_list), $filter_vals)) > 0) {
+      return 0;
+    } else {
+      return 1;
+    }
+  }
+  
+  
+  /**
+    * Filter on year. If one of selected year values match the year of the
+    * course return 0 otherwise return 1.
+    *
+    * @access private
+    * @param year
+    * @param array of years against which to match.
+    * @return 0 on successful match. 1 otherwise.
+    */
+  private function __do_year_filter($year, $chosen_years)
+  {
+    return $this->__do_single_prop_filter($year, $chosen_years);
+  }
+  
+  
+  /**
+    * Filter on school id. If the school id of one of the selected schools
+    * matches the passed school id return 0 otherwise return 1.
+    *
+    * @access private
+    * @param numerical school id
+    * @param array of school ids against which to match
+    * @return 0 on successful match. 1 otherwise.
+    */
+  private function __do_school_id_filter($school_id, $selected_schools) {
+    return $this->__do_single_prop_filter($school_id, $selected_schools);
+  }
+  
+  
+  /**
+    * Filter on matching property. If one of the filter values matches the 
+    * property, return 0 otherwise return 1. Does a strict match where types
+    * are also compared.
+    *
+    * @access private
+    * @param property to match.
+    * @param array of values to match against property.
+    * @return 0 on successful match. 1 otherwise.
+    */
+  private function __do_single_prop_filter($property, $filter_vals)
+  {
+    if (in_array($property, $filter_vals, TRUE)) {
+      return 0;
+    } else {
+      return 1;
+    }
+  }
+  
+  
+  /**
+    * Filter on term names. The term names are text values in the DB while
+    * the passed filter arguments are numeric. We first grab the term names
+    * for which the keys match against the passed filter arguments and then
+    * match against the term name property for the course.
+    */
+  private function __do_term_filter($term, $selected_terms)
+  {
+    /* Standard encoding of term names. Used for filtering by Term because
+     * passed arguments are numerical values corresponding to the array
+     * inidices of Terms below. We need to provide the names because the
+     * DB only contains the term names and no numerical Term ids.
+     */
+    $terms = array(
+      1 => "Fall",
+      2 => "Winter",
+      3 => "Spring",
+      4 => "Summer"
+      );
+    
+    if (in_array($term, array_intersect_key($terms, 
+      array_flip($selected_terms)))) {
+      return 0;
+    } else {
+      return 1;
+    } 
   }
 }
  
