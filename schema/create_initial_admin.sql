a69e75917e0449a4205a3cbf6fbdcb13e3edc112
Kevin Coffman <kwc@umich.edu>
2009-09-11 16:53:12 +0000

    Merged revisions 55211,60463,62513 via svnmerge from https://source.sakaiproject.org/contrib/ocw2/trunk
    ........
      r55211 | lotia@umich.edu | 2008-11-21 12:56:00 -0500 (Fri, 21 Nov 2008) | 4 lines
      
      added latest schema. deleted extra files.
      
      Added the OERca DB schema current at 2008/11/21 12:50 and removed the
      scores of files that were confusing matters.
    ........
      r60463 | kwc@umich.edu | 2009-05-14 15:24:02 -0400 (Thu, 14 May 2009) | 4 lines
      
      OERDEV-162
      
      Missed fixing this in the original commit(s).
    ........
      r62513 | kwc@umich.edu | 2009-08-11 11:31:29 -0400 (Tue, 11 Aug 2009) | 2 lines
      
      Add sql file with command template for creating the initial admin user.
    ........

---
+++ schema/create_initial_admin.sql
@@ -0,0 +1,26 @@
+-- phpMyAdmin SQL Dump
+-- version 2.11.7.1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Aug 11, 2009 at 11:05 AM
+-- Server version: 5.0.41
+-- PHP Version: 5.2.6
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+--
+-- Database: `OERcaDatabase`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Dumping data for table `ocw_users`
+--
+
+--
+-- NOTE:  Change "NAME", "USER_NAME", and "EMAIL" below to appropriate values
+--        before submitting this command!
+--
+INSERT INTO `ocw_users` VALUES(NULL, 'NAME', 'USER_NAME', '', 'EMAIL', 'admin', 0, NULL, '2009-01-01 00:00:00', '2009-01-01 00:00:00', '2009-01-01 00:00:00');
--- schema/zqian_oerdb_patches.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE `ocw_objects` CHANGE `action_type` `action_type` ENUM( 'Permission','Search','Fair Use', 'Re-Create','Retain: Instructor Created', 'Retain: Public Domain', 'Retain: No Copyright', 'Commission', 'Remove & Annotate' ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT 'Search'; 
-
-
-ALTER TABLE `ocw_object_replacement_questions`DROP FOREIGN KEY ocw_object_replacement_questions_ibfk_1;
-
-ALTER TABLE `ocw_object_replacement_questions`ADD CONSTRAINT `ocw_object_replacement_questions_ibfk_1` FOREIGN KEY (`object_id`) REFERENCES `ocw_object_replacements` (`object_id`);
